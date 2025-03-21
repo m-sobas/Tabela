5c21b4a3f651d6609ed4d7ff97d4f63be040829e
Mieczysław Sobaś <m.sobas@fejm.pl>
2025-03-21 21:15:32 +0100

    Add project files.

---
+++ Tabela/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Tabela.Models;
+
+namespace Tabela.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SendForm(Calendar.Models.Model model)
+    {
+        return Json(new { success = true, message = "Dane zosta³y zapisane", data = model });
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Tabela/Models/Model.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Calendar.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public List<string> Radios { get; set; }
+        public List<int> Counters { get; set; }
+        public List<IFormFile> Files { get; set; }
+    }
+}
